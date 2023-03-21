46ba51eca529b7d0483062b64c4ccec629e9895d
Iryna_Kobyzhcha <irinakobyzhcha16@ukr.net>
2023-03-21 20:32:29 +0200

    add task-1

---
+++ js/01-gallery.js
@@ -0,0 +1,92 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+// console.log(galleryItems);
+
+const galleryList =document.querySelector('.gallery');
+const pictureCard = createGalleryPictureCard (galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', pictureCard);
+galleryList.addEventListener('click', onGalleryPictureCardClick);
+
+function createGalleryPictureCard (galleryItems){
+  return galleryItems
+  .map(({preview, original, description}) => {
+return`
+<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+    `
+  })
+  .join('');
+};
+
+function onGalleryPictureCardClick (evt){
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+	}
+	const imgUrl = evt.target.dataset.source;
+
+	return showImgModal(imgUrl);
+}
+
+function showImgModal (imgUrl) {
+  const instance = basicLightbox.create(`<img src="${imgUrl}" width="1280" height="853">`, {
+  
+  onShow: (instance) => {
+    console.log('onShow', instance);
+
+    document.addEventListener("keyup", checkEsc);
+  },
+  onClose: (instance) => {
+    console.log('onClose', instance);
+
+    document.removeEventListener("keyup", checkEsc);
+  }
+})
+
+instance.show();
+
+function checkEsc({ code }) {
+  console.log(code);
+  if (code === "Escape") {
+    instance.close();
+  }
+}
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(!evt.target.classList.contains('gallery__image')){
+//   return;
+// }
+
+// const currentActiveImage = document.querySelector('gallery__link:hover');
+// if(currentActiveImage){
+//   currentActiveImage.classList.remove(':hover');
+// }
+
+// const swatchEL = evt.target;
+// const parentPictureImage = swatchEL.closest('gallery__link');
+// parentPictureImage.classList.add(':hover');
+
+// console.log(evt.target.dataset.source);
